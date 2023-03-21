900eca5d6622524c03e89c6f726ff0795ba86d4c
nogueiragabriela <nogueiragabriela93@gmail.com>
2023-03-21 09:44:33 -0300

    feat: update account

--- src/index.js
+++ src/index.js
@@ -114,12 +114,27 @@ app.post("/account", (request, response) => {
     const { customer } = request;
     const { date } = request.query;
 
-    const dateFormat = new Date(date + "00:00");
+    const dateFormat = new Date(date + " 00:00");
 
     const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === 
     new Date(dateFormat).toDateString())
 
-    return response.json(statement)
+    return response.json(statement);
  });
+
+ app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+ });
+
+ app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+ })
 
  app.listen(3333);
